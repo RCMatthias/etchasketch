9a47e4724616cdb822a3694dde9da6a15ce84ad8
RCMatthias <matthias.vdvelde@hotmail.com>
2022-10-18 18:42:36 +0200

    fix problem with grid resize

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const container = document.getElementById("container");
-const userInput = document.getElementById("quantity");
-
+const container = document.querySelector(".container");
+const userInput = document.getElementById("quantity") ;
+const resetButton = document.querySelector(".reset");
 
 //creates a default grid size, values can be changed later by user with slider e.g.
-createGrid();
-
 function createGrid () {
     for (let i = 0; i < 256; i++) {
         const div = document.createElement("div");
         div.classList.add('square');
         container.appendChild(div);
-    }
+    };
 };
+userInput.addEventListener("change", updateGrid);
 
 function updateGrid () {
     //clear innerhtml from default grid container so we don't continuously stack new squares on old ones.
@@ -25,23 +24,32 @@ function updateGrid () {
         "grid-template-rows",
         `repeat(${userInput.value}, 2fr)`
     );
-
-
     //build and append as many squares as needed to fill columns & rows
+    for (let i =0; i<(userInput.value * userInput.value);i++){
+            const div = document.createElement("div");
+            div.classList.add("square");
+            container.appendChild(div);
+        }
+};
 
 
+
+//change color on hover
 const square = document.querySelector("div");
-square.addEventListener("mouseover", function(event){
-    event.target.classList.replace("square", "color")
+square.addEventListener("mouseover", function(e){
+    e.target.classList.replace("square", "colored")
 });
 
 
-//flexbox the resulting divs in CSS (classes)
-//borders&margins using em/rem? 
+//reset button to basic 16x16 grid
+resetButton.addEventListener("click", function(){
+    container.innerHTML="";
+    userInput.value="";
+    container.style.setProperty("grid-template-columns", `repeat(16, 2fr)`);
+    container.style.setProperty("grid-template-rows", `repeat(16, 2fr)`);
+    createGrid();
+}) ;
 
+createGrid();
 
-//individual div in grid changes (bg) color when a mouse hover enters 
-/* cells.addEventListener('hov') */
-
-/* mouseenter
-mouseleave  */
+//color select button (change color of "color" class)
